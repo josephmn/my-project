a027e5d2f5d8e7e04df63afa1452235600c7277b
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2025-09-22 18:49:42 -0500

    agregando eventos en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,9 @@ import { HeaderComponent } from './components/header/header.component';
 })
 export class AppComponent {
   title = 'my-project';
+  number: number = 0;
+
+  addOne() {
+    this.number++;
+  }
 }
