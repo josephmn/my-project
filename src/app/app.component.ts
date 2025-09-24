88a6e432d6f8f48509d3fabb64c4996c912b25fb
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2025-09-24 00:29:02 -0500

    trabajando con lista de objetos en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  userName: string = 'Joseph';
-  number: number = 0;
-  condition: boolean = false;
-  condition2: string = "";
-
   movies: string[] = ['Star Wars', 'Game of Thron', 'Back to future', 'Angry Birds'];
-
-  addOne() {
-    this.number++;
-  }
+  animals: any = [
+    {
+        id: 1,
+        name: "dog",
+        img: "https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg"
+    },
+    {
+        id: 2,
+        name: "cat",
+        img: "https://purina.com.pe/sites/default/files/2022-10/Que_debes_saber_antes_de_adoptar_un_gatito.jpg"
+    },
+    {
+        id: 3,
+        name: "bird",
+        img: "https://cdn0.expertoanimal.com/es/posts/3/3/8/caracteristicas_de_las_aves_24833_600.jpg"
+    },
+];
 }
---
+++ src/data/animals.js
@@ -0,0 +1,17 @@
+animals: any = [
+    {
+        id: 1,
+        name: "dog",
+        img: "https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg"
+    },
+    {
+        id: 2,
+        name: "cat",
+        img: "https://purina.com.pe/sites/default/files/2022-10/Que_debes_saber_antes_de_adoptar_un_gatito.jpg"
+    },
+    {
+        id: 3,
+        name: "bird",
+        img: "https://cdn0.expertoanimal.com/es/posts/3/3/8/caracteristicas_de_las_aves_24833_600.jpg"
+    },,
+];
