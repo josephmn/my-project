4904b671d01f4c7cedb8dc34d8250f700893647b
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2025-09-26 12:46:46 -0500

    trabajando con ngClass en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,21 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import Product from './models/product.models';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgClass,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  userName: string;
-  myNumber: number;
-  myBoolean: boolean;
-  product: Product;
+  isDark: boolean = true;
 
-  constructor() {
-    this.userName = "Adrián";
-    this.myNumber = 10;
-    this.myBoolean = true;
-
-    this.product = {
-      name: "Computer",
-      price: 1000,
-      isForSale: true
-    }
+  toggleDark() {
+    this.isDark = !this.isDark;
   }
 }
